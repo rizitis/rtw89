a8e6c1c1fef2c807e260b0feea75fcb1fa77c664
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2025-03-19 23:13:34 +0200

    Add files via upload

---
+++ doinst.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+fw="rtw8851b_fw.bin rtw8852a_fw.bin rtw8852b_fw-1.bin rtw8852bt_fw.bin rtw8852c_fw-1.bin rtw8922a_fw-3.bin"
+
+for fws in $fw; do
+    if [ -f "/lib/firmware/$fws" ]; then
+        sudo rm -f "/lib/firmware/$fws"
+    fi
+
+    sudo ln -s "/usr/src/rtw89-firmware/$fws" "/lib/firmware/$fws"
+done
